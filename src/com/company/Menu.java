1ed84acff608af518a90ca20431403ca3c19dc4b
Mathis <mathis@mbp-de-mathis.edu.sir>
2020-10-13 16:19:12 +0200

    Ajout choix

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -10,7 +10,7 @@ public class Menu{
         Scanner choix = new Scanner(System.in);
         int choixUtilisateur;
 
-        System.out.println("nom"+ \+"prenom");
+        System.out.println("nom"+"prenom");
 
         choixUtilisateur = choix.nextInt();
         switch (choixUtilisateur) {
