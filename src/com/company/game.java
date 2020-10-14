6ef7aeed9359a6012f8dccbed36f831901cb01bb
Yanis JEDRZEJCZAK <yanisjdz@outlook.fr>
2020-10-14 10:33:52 +0200

    traduction + changement affichagePlateau

--- src/com/company/game.java
+++ src/com/company/game.java
@@ -8,11 +8,11 @@ public class game {
      * Demande à l'utilisateur de choisir son pseudo
      * @return le pseudo du joueur (String)
      */
-    public static String Players(){
+    public static String Joueurs(){
         Scanner player = new Scanner(System.in);
 
-        String players = player.next();
-        return players;
+        String joueur = player.next();
+        return joueur;
     }
 
     /**
@@ -20,12 +20,12 @@ public class game {
      * @param pseudo1
      * @param pseudo2
      */
-    public static void random(String pseudo1, String pseudo2){
+    public static void premierAleatoire(String pseudo1, String pseudo2){
         int min = 0; int max = 2;
 
-        int random = (int)(Math.random()*(min-max))+max;
+        int aleatoire = (int)(Math.random()*(min-max))+max;
 
-        if(random == 1){
+        if(aleatoire == 1){
             System.out.println("C'est à "+pseudo1+" de jouer en premier");
         }else {
             System.out.println("C'est à "+pseudo2+" de jouer en premier");
@@ -37,17 +37,18 @@ public class game {
      * Affichage tableau 11x10
      */
     public static void affichagePlateau(){
-        String[][] board = new String[10][11];
+        String[][] plateau = new String[10][11];
         int i; int j;
 
         for (i=0;i<10;i++){
             for (j=0;j<11;j++){
-                board[i][j] = "X ";
-                System.out.print(board[i][j]);
+                plateau[i][j] = "☐ ";
+                System.out.print(plateau[i][j]);
             }
             System.out.println();
         }
     }
+
 
 
     /**
@@ -59,12 +60,11 @@ public class game {
         String pseudo1; String pseudo2;
 
         System.out.println("Entrez le pseudo du joueur 1:");
-        pseudo1= Players();
+        pseudo1= Joueurs();
         System.out.println("Entrez le pseudo du joueur 2:");
-        pseudo2 = Players();
+        pseudo2 = Joueurs();
 
         affichagePlateau();
-
-        random(pseudo1,pseudo2);
+        premierAleatoire(pseudo1,pseudo2);
     }
 }
