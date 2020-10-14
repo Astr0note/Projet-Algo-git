c854e7128f8749796537fe24121b5261ef6c757a
Yanis JEDRZEJCZAK <yanisjdz@outlook.fr>
2020-10-14 17:37:15 +0200

    Prend en compte les bords

--- src/com/company/Bloquage.java
+++ src/com/company/Bloquage.java
@@ -15,47 +15,79 @@ public class Bloquage {
         String joueur2 = "O ";
         String joueur1 = "X ";
         String caseDetruite = "# ";
-
+        String caseGauche = "";
+        String caseDroit = "";
+        String caseHaute = "";
+        String caseBas = "";
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 11; j++) {
                 if (Plateau[i][j] == joueur1) {
-                    String caseBas = Plateau[i][j + 1];
-                    String caseGauche = Plateau[i - 1][j];
-                    String caseDroite = Plateau[i + 1][j];
-                    String caseHaut = Plateau[i][j - 1];
-                    if ((caseDroite == caseDetruite) || (caseDroite == joueur2)) {
-                        if ((caseBas == caseDetruite) || (caseBas == joueur2)) {
-                            if ((caseGauche == caseDetruite) || (caseGauche == joueur2)) {
-                                if ((caseHaut == caseDetruite) || (caseHaut == joueur2)) {
+                    if(j!= 10 && j!= 0 && i!=9 && i!=0){
+                         caseDroit = Plateau[i][j + 1];
+                         caseGauche = Plateau[i][j - 1];
+                         caseHaute = Plateau[i - 1][j];
+                         caseBas = Plateau[i + 1][j];
+                    }else {
+                        if (j == 10) {
+                             caseDroit = caseDetruite;
+                             caseGauche = Plateau[i][j - 1];
+                             caseHaute = Plateau[i - 1][j];
+                             caseBas = Plateau[i + 1][j];
+                        }
+                        if (j == 0) {
+                             caseGauche = caseDetruite;
+                             caseHaute = Plateau[i - 1][j];
+                             caseBas = Plateau[i + 1][j];
+                             caseDroit = Plateau[i][j + 1];
+
+                        }
+                        if (i == 9) {
+                             caseBas = caseDetruite;
+                             caseDroit = Plateau[i][j + 1];
+                             caseHaute = Plateau[i - 1][j];
+                             caseGauche = Plateau[i][j - 1];
+                        }
+                        if (i == 0) {
+                             caseHaute = caseDetruite;
+                             caseDroit = Plateau[i][j + 1];
+                             caseGauche = Plateau[i][j - 1];
+                             caseBas = Plateau[i + 1][j];
+                        }
+                    }
+
+                    if ((caseBas == caseDetruite) || (caseBas == joueur2)) {
+                        if ((caseDroit == caseDetruite) || (caseDroit == joueur2)) {
+                            if ((caseHaute == caseDetruite) || (caseHaute == joueur2)) {
+                                if ((caseGauche == caseDetruite) || (caseGauche == joueur2)) {
                                     bloque = true;
                                 }
                             }
                         }
                     }
-                    if ((caseBas == caseDetruite) || (caseBas == joueur2)) {
-                        if ((caseDroite == caseDetruite) || (caseDroite == joueur2)) {
-                            if ((caseGauche == caseDetruite) || (caseGauche == joueur2)) {
-                                if ((caseHaut == caseDetruite) || (caseHaut == joueur2)) {
+                    if ((caseDroit == caseDetruite) || (caseDroit == joueur2)) {
+                        if ((caseBas == caseDetruite) || (caseBas == joueur2)) {
+                            if ((caseHaute == caseDetruite) || (caseHaute == joueur2)) {
+                                if ((caseGauche == caseDetruite) || (caseGauche == joueur2)) {
                                     bloque = true;
                                 }
+                            }
+                        }
+                    }
+                    if ((caseHaute == caseDetruite) || (caseHaute == joueur2)) {
+                        if ((caseDroit == caseDetruite) || (caseDroit == joueur2)) {
+                            if ((caseBas == caseDetruite) || (caseBas == joueur2)) {
+                                if ((caseGauche == caseDetruite) || (caseGauche == joueur2)) {
+
+                                    bloque = true;
+                                }
+
                             }
                         }
                     }
                     if ((caseGauche == caseDetruite) || (caseGauche == joueur2)) {
-                        if ((caseBas == caseDetruite) || (caseBas == joueur2)) {
-                            if ((caseDroite == caseDetruite) || (caseDroite == joueur2)) {
-                                if ((caseHaut == caseDetruite) || (caseHaut == joueur2)) {
-
-                                    bloque = true;
-                                }
-
-                            }
-                        }
-                    }
-                    if ((caseHaut == caseDetruite) || (caseHaut == joueur2)) {
-                        if ((caseBas == caseDetruite) || (caseBas == joueur2)) {
-                            if ((caseGauche == caseDetruite) || (caseGauche == joueur2)) {
-                                if ((caseDroite == caseDetruite) || (caseDroite == joueur2)) {
+                        if ((caseDroit == caseDetruite) || (caseDroit == joueur2)) {
+                            if ((caseHaute == caseDetruite) || (caseHaute == joueur2)) {
+                                if ((caseBas == caseDetruite) || (caseBas == joueur2)) {
 
                                     bloque = true;
                                 }
@@ -67,43 +99,71 @@ public class Bloquage {
 
 
                 if (Plateau[i][j] == joueur2) {
-                    String caseBas = Plateau[i][j + 1];
-                    String caseGauche = Plateau[i - 1][j];
-                    String caseDroite = Plateau[i + 1][j];
-                    String caseHaut = Plateau[i][j - 1];
-                    if ((caseDroite == caseDetruite) || (caseDroite == joueur1)) {
-                        if ((caseBas == caseDetruite) || (caseBas == joueur1)) {
-                            if ((caseGauche == caseDetruite) || (caseGauche == joueur1)) {
-                                if ((caseHaut == caseDetruite) || (caseHaut == joueur1)) {
+                    if(j!= 10 && j!= 0 && i!=9 && i!=0){
+                        caseDroit = Plateau[i][j + 1];
+                        caseGauche = Plateau[i][j - 1];
+                        caseHaute = Plateau[i - 1][j];
+                        caseBas = Plateau[i + 1][j];
+                    }else {
+                        if (j == 10) {
+                            caseDroit = caseDetruite;
+                            caseGauche = Plateau[i][j - 1];
+                            caseHaute = Plateau[i - 1][j];
+                            caseBas = Plateau[i + 1][j];
+                        }
+                        if (j == 0) {
+                            caseGauche = caseDetruite;
+                            caseHaute = Plateau[i - 1][j];
+                            caseBas = Plateau[i + 1][j];
+                            caseDroit = Plateau[i][j + 1];
+
+                        }
+                        if (i == 9) {
+                            caseBas = caseDetruite;
+                            caseDroit = Plateau[i][j + 1];
+                            caseHaute = Plateau[i - 1][j];
+                            caseGauche = Plateau[i][j - 1];
+                        }
+                        if (i == 0) {
+                            caseHaute = caseDetruite;
+                            caseDroit = Plateau[i][j + 1];
+                            caseGauche = Plateau[i][j - 1];
+                            caseBas = Plateau[i + 1][j];
+                        }
+                    }
+                    if ((caseBas == caseDetruite) || (caseBas == joueur1)) {
+                        if ((caseDroit == caseDetruite) || (caseDroit == joueur1)) {
+                            if ((caseHaute == caseDetruite) || (caseHaute == joueur1)) {
+                                if ((caseGauche == caseDetruite) || (caseGauche == joueur1)) {
                                     bloque = true;
                                 }
                             }
                         }
                     }
-                    if ((caseBas == caseDetruite) || (caseBas == joueur1)) {
-                        if ((caseDroite == caseDetruite) || (caseDroite == joueur1)) {
-                            if ((caseGauche == caseDetruite) || (caseGauche == joueur1)) {
-                                if ((caseHaut == caseDetruite) || (caseHaut == joueur1)) {
+                    if ((caseDroit == caseDetruite) || (caseDroit == joueur1)) {
+                        if ((caseBas == caseDetruite) || (caseBas == joueur1)) {
+                            if ((caseHaute == caseDetruite) || (caseHaute == joueur1)) {
+                                if ((caseGauche == caseDetruite) || (caseGauche == joueur1)) {
                                     bloque = true;
                                 }
+                            }
+                        }
+                    }
+                    if ((caseHaute == caseDetruite) || (caseHaute == joueur1)) {
+                        if ((caseDroit == caseDetruite) || (caseDroit == joueur1)) {
+                            if ((caseBas == caseDetruite) || (caseBas == joueur1)) {
+                                if ((caseGauche == caseDetruite) || (caseGauche == joueur1)) {
+
+                                    bloque = true;
+                                }
+
                             }
                         }
                     }
                     if ((caseGauche == caseDetruite) || (caseGauche == joueur1)) {
-                        if ((caseBas == caseDetruite) || (caseBas == joueur1)) {
-                            if ((caseDroite == caseDetruite) || (caseDroite == joueur1)) {
-                                if ((caseHaut == caseDetruite) || (caseHaut == joueur1)) {
-
-                                    bloque = true;
-                                }
-
-                            }
-                        }
-                    }
-                    if ((caseHaut == caseDetruite) || (caseHaut == joueur1)) {
-                        if ((caseBas == caseDetruite) || (caseBas == joueur1)) {
-                            if ((caseGauche == caseDetruite) || (caseGauche == joueur1)) {
-                                if ((caseDroite == caseDetruite) || (caseDroite == joueur1)) {
+                        if ((caseDroit == caseDetruite) || (caseDroit == joueur1)) {
+                            if ((caseHaute == caseDetruite) || (caseHaute == joueur1)) {
+                                if ((caseBas == caseDetruite) || (caseBas == joueur1)) {
 
                                     bloque = true;
                                 }
