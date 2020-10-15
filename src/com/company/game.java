4e8d3e25205a8a4b0086c5462ff2c3ab1fa77ab0
Yanis JEDRZEJCZAK <yanisjdz@outlook.fr>
2020-10-15 08:21:25 +0200

    Ajout sauvegarde pseudo joueur1/2 + commentaires

--- src/com/company/game.java
+++ src/com/company/game.java
@@ -19,15 +19,17 @@ public class game {
      * @param pseudo1
      * @param pseudo2
      */
-    public static void premierAleatoire(String pseudo1, String pseudo2){
+    public static String premierAleatoire(String pseudo1, String pseudo2){
         int min = 0; int max = 2;
 
         int aleatoire = (int)(Math.random()*(min-max))+max;
 
         if(aleatoire == 1){
             System.out.println("C'est à "+pseudo1+" de jouer en premier");
+            return pseudo1;
         }else {
             System.out.println("C'est à "+pseudo2+" de jouer en premier");
+            return pseudo2;
         }
 
     }
@@ -75,18 +77,21 @@ public class game {
      * Va exectuer dans un ordre précis les actions de déplacement du joueur
      * @param Plateau
      */
-    public static void deplacement(String[][] Plateau){
+    public static void deplacement(String[][] Plateau,String joueur1, String joueur2, int tour){
 
         boolean bloque;
         bloque = Bloquage.estBloque(Plateau);
-        if(bloque == true){
-            //Fin de partie
+        if(bloque == true){ //Fin de partie
             System.out.println("Le joueur est bloqué!");
             System.out.println("Fin de la partie");
             Menu.choix();
-        }else {
-            System.out.println("Pas bloqué");
-            //Le joueur peut jouer
+        }else { //Le joueur peut jouer
+
+            if (tour%2 == 1){ //Les tours impairs = joueur1. Les tours pairs = joueur2
+                System.out.println("C'est à "+joueur1+" de jouer");
+            }else{
+                System.out.println("C'est à "+joueur2+" de jouer");
+            }
 
             //here : Fonction déplacement de Mathis
             Plateau = Mouvement.mouvemement(Plateau);
@@ -123,21 +128,34 @@ public class game {
      */
     public static void base(){
 
-        String pseudo1; String pseudo2; String[][] plateau;boolean enCours = true;
+        String pseudo1; String pseudo2;String premierJoueur;
+        String joueur1; String joueur2;
+        String[][] plateau;
+        boolean enCours = true;
         int x1 = 4; int y1=5 ;      // Position initiale des deux joueurs
         int x2 = 5; int y2 = 5;
+        int tour = 1;
 
         System.out.println("Entrez le pseudo du joueur 1:");
         pseudo1= Joueurs();
         System.out.println("Entrez le pseudo du joueur 2:");
         pseudo2 = Joueurs();
 
+        premierJoueur = premierAleatoire(pseudo1,pseudo2);
         plateau = fontionPlateau(x1,y1,x2,y2);
-        premierAleatoire(pseudo1,pseudo2);
 
+        if(premierJoueur == pseudo1){   //En fonction du tirage aléatoire,
+            joueur1 = pseudo1;          //Joueur1 et Joueur2 sont définis
+            joueur2 = pseudo2;
+        }else{
+            joueur1 = pseudo2;
+            joueur2 = pseudo1;
+        }
 
-            //A faire boucler d'une certaines manière....
-            deplacement(plateau);
+        while(tour>0) {
+            deplacement(plateau, joueur1, joueur2, tour);
+            tour++;
+        }
 
 
     }
