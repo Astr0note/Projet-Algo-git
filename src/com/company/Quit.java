3a3a0aa9289ccfc6ff0d2604b2e60f65a12930f5
Mathis <mathisouchikh@gmail.com>
2020-10-15 14:05:02 +0200

    Update note

--- src/com/company/Quit.java
+++ src/com/company/Quit.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Quit {
     public static void quittez() {
-        //On va créer une fonction pour que l'utilisateur puisse quittez le jeu quand il le souhaite.
-        //Il devra dabord lancer une partie pour pouvoir quittez et pourra le faire dès le premier tour si il tape "quittez
+        //On met la fonction quittez dans un nouveau fichier pour qu'il soit plus facile à appelé
+        //Il devra dabord lancer une partie pour pouvoir quittez et pourra le faire dès le premier tour si il tape "M"
             Menu.choix();
     }
 }
