d5c64d99aa7ad7f3c723d58abcf9efbde759e748
Yanis JEDRZEJCZAK <yanisjdz@outlook.fr>
2020-10-15 07:57:13 +0200

    Ajout commentaires

--- src/com/company/Bloquage.java
+++ src/com/company/Bloquage.java
@@ -11,6 +11,8 @@ public class Bloquage {
      * @return un boolean. true = joueur bloqué.
      */
     public static boolean estBloque(String[][] Plateau) {
+
+        //Initialisation de toutes les variables
         boolean bloque = false;
         String joueur2 = "O ";
         String joueur1 = "X ";
@@ -19,6 +21,8 @@ public class Bloquage {
         String caseDroit = "";
         String caseHaute = "";
         String caseBas = "";
+
+        //Vérification si le joueur est proche des bords du tableau
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 11; j++) {
                 if (Plateau[i][j] == joueur1) {
@@ -27,7 +31,8 @@ public class Bloquage {
                          caseGauche = Plateau[i][j - 1];
                          caseHaute = Plateau[i - 1][j];
                          caseBas = Plateau[i + 1][j];
-                    }else {
+                    }else {     //Si le joueur est proche d'un bord, la case adjacente est considérée comme une case détruite, en fonction de
+                                //la position du joueur
                         if (j == 10) {
                              caseDroit = caseDetruite;
                              caseGauche = Plateau[i][j - 1];
@@ -55,6 +60,8 @@ public class Bloquage {
                         }
                     }
 
+
+                    //Comparaison pour chaque coté du joueurs, voir s'il est bloqué
                     if ((caseBas == caseDetruite) || (caseBas == joueur2)) {
                         if ((caseDroit == caseDetruite) || (caseDroit == joueur2)) {
                             if ((caseHaute == caseDetruite) || (caseHaute == joueur2)) {
@@ -97,14 +104,15 @@ public class Bloquage {
                 }
 
 
-
+                //Vérification si le joueur est proche des bords du tableau
                 if (Plateau[i][j] == joueur2) {
                     if(j!= 10 && j!= 0 && i!=9 && i!=0){
                         caseDroit = Plateau[i][j + 1];
                         caseGauche = Plateau[i][j - 1];
                         caseHaute = Plateau[i - 1][j];
                         caseBas = Plateau[i + 1][j];
-                    }else {
+                    }else {     //Si le joueur est proche d'un bord, la case adjacente est considérée comme une case détruite, en fonction de
+                                //la position du joueur
                         if (j == 10) {
                             caseDroit = caseDetruite;
                             caseGauche = Plateau[i][j - 1];
@@ -131,6 +139,8 @@ public class Bloquage {
                             caseBas = Plateau[i + 1][j];
                         }
                     }
+
+                    //Comparaison pour chaque coté du joueurs, voir s'il est bloqué
                     if ((caseBas == caseDetruite) || (caseBas == joueur1)) {
                         if ((caseDroit == caseDetruite) || (caseDroit == joueur1)) {
                             if ((caseHaute == caseDetruite) || (caseHaute == joueur1)) {
